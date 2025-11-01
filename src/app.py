6c35157adbe09fce4c56e4999db387618040e438
Bartosz <bartoszradz@gmail.com>
2025-11-01 02:32:53 +0100

    zmiana komunikatu startowego

--- src/app.py
+++ src/app.py
@@ -5,4 +5,5 @@ load_dotenv()  # wczyta zmienne z .env
 
 def uruchom():
     app_name = os.getenv("APP_NAME", "Aplikacja domyślna")
-    print(f"Startuję: {app_name}")
+    print(f"Startuję aplikację: {app_name}")
+    print("Wersja developerska")
