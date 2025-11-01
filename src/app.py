59019f27f068f1f27cb1d55ade59401d80393bf5
Bartosz <bartoszradz@gmail.com>
2025-11-01 02:06:28 +0100

    Initial project setup

---
+++ src/app.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # wczyta zmienne z .env
+
+def uruchom():
+    app_name = os.getenv("APP_NAME", "Aplikacja domyślna")
+    print(f"Startuję: {app_name}")
